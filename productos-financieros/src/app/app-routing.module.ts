35cbf5736d837b03af76745828e07985d4889cc0
Christian <christian_benitez1397@hotmail.com>
2024-11-07 00:32:21 -0500

    table and register product FE

--- productos-financieros/src/app/app-routing.module.ts
+++ productos-financieros/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
+import { FormRegisterComponent } from './form-register/form-register.component';
 
 const routes: Routes = [
   {path: 'bp/products', component: ProductListComponent},
-  {path: 'bp/products', component: ProductListComponent}
+  {path: 'bp/products/create', component: FormRegisterComponent}
 ];
 
 @NgModule({
